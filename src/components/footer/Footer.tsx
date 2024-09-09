995b5b1e06dc83009e7b8ce90dbdfb70040c1f29
lucas_souza_ribeiro <lucas.srs.2022@gmail.com>
2024-09-09 13:01:23 -0300

    footer e navbar

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div className='bg-orange-300 h-24 flex items-center justify-center'>
+        <ul className='flex items-center justify-center font-semibold uppercase *:mx-2 hover:*:underline'>
+            <li>sobre</li>
+            <li>gitHub</li>
+            <li>linkedin</li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import { List } from "@phosphor-icons/react";
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const btnMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <div className='bg-orange-300 flex justify-center items-center shadow-md md:h-28 md:justify-between px-4'>
+                <img src={logo} alt="logo do website Daylist" className="w-12 h-12 rounded-full border-2 border-white shadow-lg order-2 md:h-24 md:w-24" />
+                <div className={`absolute left-3 top-3 flex flex-col md:relative md:order-2 md:top-0`}>
+                    <button onClick={btnMenu} className="text-black md:hidden">
+                        <List size={32} weight={`${isOpen ? "light" : "bold"}`} />
+                    </button>
+                    <ul className={`flex flex-col justify-between px-5 *:rounded-sm *:py-4 hover:*:underline  hover:*:text-white *:transition-all *:duration-300 transition-all duration-400 ease-in-out bg-orange-400 font-medium uppercase shadow-lg rounded-lg transform ${isOpen ? `h-auto opacity-100` : `h-0 opacity-0 pointer-events-none`}
+                                    md:h-auto md:opacity-100 md:flex-row md:*:px-2 md:bg-transparent md:shadow-none md:text-2xl md:hover:*:shadow-md`}>
+                        <li>Daylist</li>
+                        <li>Perfil</li>
+                        <li>Login</li>
+                        <li>Logout</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
